2b5ef6cad386983246d3b0ea9e60eea75e367552
lclblack <362450790@qq.com>
2016-01-18 09:05:46 +0800

    add admin.php

---
+++ admin/mySmarty/index.class.php
@@ -0,0 +1,12 @@
+<?php
+class index extends MySmarty {
+    public function __construct() {
+        parent::__construct();
+    }
+    public function init() {
+        parent::init();
+    }
+    public function index() {
+        echo "hello world!";
+    }
+}
